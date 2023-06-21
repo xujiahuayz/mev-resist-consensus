b60de2db696ce57a2360fc9cb874ea5d5f0da457
TammyQAQ <tammywang20020305@gmail.com>
2023-06-22 00:33:10 +0100

    added more agents

--- market_env/env.py
+++ market_env/env.py
@@ -7,12 +7,27 @@ class DefiEnv:
         users: dict[str, User] | None = None,
         proposers: dict[str, Proposer] | None = None,
         builders: dict[str, Builder] | None = None,
-        prices: PriceDict | None = None,
+        mempools: Mempool | None = None,
+        blocks: Block | None = None,
     ):
+
         if users is None:
             users = {}
         if proposers is None:
             proposers = {}
         if builders is None:
             builders = {}
-          
+        if mempools is None:
+            mempools = {}
+        if blocks is None:
+            blocks = {}
+
+        self.users = users
+        self.proposers = proposers
+        self.builders = builders
+        self.mempools = mempools
+        self.blocks = blocks
+
+
+        
+        
