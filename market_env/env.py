88d2b41d30e711f96aca0dfa6116f8dbc7376c1d
TammyQAQ <tammywang20020305@gmail.com>
2023-07-03 10:20:08 +0100

    added type

--- market_env/env.py
+++ market_env/env.py
@@ -34,40 +34,40 @@ class ChainEnv:
 
 
 class User:
-    def __init__(self, user_id, env: ChainEnv):
+    def __init__(self, user_id, env: ChainEnv) -> None:
         self.user_id = user_id
         self.env = env
 
     # users initiate transactions to a recipient
-    def create_transaction(self, transaction_id, amount, recipient):
+    def create_transaction(self, transaction_id, amount, recipient) -> "Transaction":
         transaction = Transaction(transaction_id, amount, recipient)
         return transaction
 
     # users send transactions to mempool
-    def send_transaction(self, transaction):
+    def send_transaction(self, transaction) -> None:
         self.env.mempools.add_transaction(transaction)
 
 
 class Proposer:
-    def __init__(self, proposer_id):
+    def __init__(self, proposer_id) -> None:
         self.proposer_id = proposer_id
 
     # select the block with the highest pay
-    def select_block(self, block_id, builder_blocks):
+    def select_block(self, block_id, builder_blocks) -> "Block":
         selected_block = max(builder_blocks, key=lambda block: block.get_pay())
         return selected_block
 
 
 class Builder:
-    def __init__(self, builder_id):
+    def __init__(self, builder_id) -> None:
         self.builder_id = builder_id
         self.mempool = None
 
-    def set_mempool(self, mempool):
+    def set_mempool(self, mempool) -> None:
         self.mempool = mempool
 
     # check mempoool, order the transactions, and build the block
-    def build_block(self, block_id):
+    def build_block(self, block_id) -> "Block":
         if self.mempool is None:
             raise ValueError("Mempool not set for the builder.")
 
@@ -80,29 +80,31 @@ class Builder:
         return block
 
     # order transactions by gas usage
-    def order_transactions(self, transactions):
+    def order_transactions(
+        self, transactions: List["Transaction"]
+    ) -> List["Transaction"]:
         ordered_transactions = sorted(transactions, key=lambda x: x.gas_usage)
         return ordered_transactions
 
 
 class Mempool:
-    def __init__(self):
-        self.transactions = []
+    def __init__(self) -> None:
+        self.transactions: List["Transaction"] = []
 
-    def add_transaction(self, transaction):
+    def add_transaction(self, transaction: "Transaction") -> None:
         self.transactions.append(transaction)
 
-    def get_transactions(self):
+    def get_transactions(self) -> List["Transaction"]:
         return self.transactions
 
 
 class Block:
-    def __init__(self, block_id):
+    def __init__(self, block_id) -> None:
         self.block_id = block_id
-        self.transactions = []
+        self.transactions: List["Transaction"] = []
 
-    def add_transaction(self, transaction):
+    def add_transaction(self, transaction: "Transaction") -> None:
         self.transactions.append(transaction)
 
-    def get_transactions(self):
+    def get_transactions(self) -> List["Transaction"]:
         return self.transactions
