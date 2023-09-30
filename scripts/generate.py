7e0225ce7f8b172d296d02fbf3adb8a52155a32f
TammyQAQ <tammywang20020305@gmail.com>
2023-09-30 13:36:43 +0100

    generate trsnsaction

--- scripts/generate.py
+++ scripts/generate.py
@@ -16,6 +16,21 @@ def generate_accounts(num_accounts):
         accounts.append(account)
     return accounts
 
+def generate_transactions(accounts, num_transactions):
+    transactions = []
+    for _ in range(num_transactions):
+        sender = random.choice(accounts)
+        recipient = random.choice(accounts)
+        while sender == recipient:
+            recipient = random.choice(accounts)
+        
+        amount = random.uniform(1.0, 10.0)  
+        base_fee = BASE_FEE
+        priority_fee = random.uniform(0.0, 5.0)  
+        transaction = sender.create_transaction(recipient, amount, base_fee, priority_fee)
+        transactions.append(transaction)
+    return transactions
+
 def generate_builders(num_builders):
     builders = []
     for i in range(num_builders):
@@ -31,9 +46,28 @@ def generate_proposers(num_proposers):
     return proposers
 
 num_accounts = 200
+num_transactions = 100
 initial_balance = 100.0
 num_builders = 20
 num_proposers = 20
 
-builders = generate_builders(num_builders)
-proposers = generate_proposers(num_proposers)
+
+
+if __name__ == "__main__":
+    accounts = generate_accounts(num_accounts)
+    transactions = generate_transactions(accounts, num_transactions)
+    builders = generate_builders(num_builders)
+    proposers = generate_proposers(num_proposers)
+
+    for account in accounts:
+        print(account.address, account.balance)
+    for transaction in transactions:
+        print(
+            f"Transaction ID: {transaction.transaction_id}, Sender: {transaction.sender}, "
+            f"Recipient: {transaction.recipient}, Amount: {transaction.amount}, "
+            f"Base Fee: {transaction.base_fee}, Priority Fee: {transaction.priority_fee}"
+        )
+    for builder in builders:
+        print(builder.address, builder.balance)
+    for proposer in proposers:
+        print(proposer.address, proposer.balance)
