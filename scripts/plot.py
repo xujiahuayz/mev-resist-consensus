ffdbc555678bdcf4acd0a810caa14b53dde71a95
TammyQAQ <tammywang20020305@gmail.com>
2023-11-01 11:09:42 +0000

    minor changes

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,8 +3,8 @@ import numpy as np
 
 from scripts.generate import total_proposer_balance, total_builder_balance
 
-
-def plot_distribution(total_proposer_balance: list[float], total_builder_balance: list[float], initial_balance: float):
+def plot_distribution(total_proposer_balance: list[float], total_builder_balance: list[float],
+                      initial_balance: float):
     block_numbers = np.arange(len(total_proposer_balance))  # Create an array of block numbers
 
     # Calculate the total profit (ignoring initial balances) for each block
@@ -19,15 +19,17 @@ def plot_distribution(total_proposer_balance: list[float], total_builder_balance
     builder_percentage = (total_profit_builder / total_profit) * 100
 
     plt.figure(figsize=(10, 6))
-    plt.stackplot(block_numbers, proposer_percentage, builder_percentage, labels=['Proposer Profit', 'Builder Profit'], alpha=0.7)
-    plt.plot(block_numbers, np.ones_like(block_numbers) * 100, color='black', linestyle='--', label='Total (100%)')
+    plt.stackplot(block_numbers, proposer_percentage, builder_percentage,
+                  labels=['Proposer Profit', 'Builder Profit'], alpha=0.7)
+    plt.plot(block_numbers, np.ones_like(block_numbers) * 100, color='black',
+             linestyle='--', label='Total (100%)')
     plt.xlabel('Block Number')
     plt.ylabel('Percentage')
     plt.legend(loc='upper left')
     plt.grid(False)
-    plt.title('Percentage Distribution of Proposer and Builder Profits Over Blocks (Ignoring Initial Balances)')
+    plt.title('Percentage Distribution of Proposer and Builder Profits Over Blocks')
     plt.show()
-    plt.savefig('figures/profit_distribution.pdf')
+    plt.savefig('./profit_distribution.pdf')
 
 if __name__ == "__main__":
     plot_distribution(total_proposer_balance, total_builder_balance, 1000.0)
