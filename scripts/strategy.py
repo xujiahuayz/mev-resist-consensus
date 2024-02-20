0c12304466321fe5215009f4890cd239814b7f3f
TammyQAQ <tammywang20020305@gmail.com>
2024-02-20 11:19:51 +0000

    plot

--- scripts/strategy.py
+++ scripts/strategy.py
@@ -5,13 +5,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from plot import Plotting
 
 random.seed(42)
 
 # Constants
 NUM_BUILDERS = 20
 NUM_BLOCKS = 100
-STRATEGIES = ['fraction_based', 'reactive', 'historical', 'last_minute', 'bluff', 'combined']
+STRATEGIES = ['fraction_based', 'reactive', 'historical', 'last_minute', 'bluff']
 CHANGE_STRATEGY_RATE = 0.2
 
 class Builder:
@@ -47,9 +48,20 @@ class Builder:
             return 0
         elif self.strategy == 'bluff':
             return block_value * (1 if counter < 22 else 0.5)
-        elif self.strategy == 'combined':
-            pass
 
+    def intra_rankings(self):
+        intra_block_rankings = []
+
+        for block_bid_his in self.block_bid_his:
+            all_bids = [bid for counter_bids in block_bid_his for bid in counter_bids.values()]
+
+            sorted_bids = sorted(all_bids, reverse=True)
+            winning_bid = max(all_bids)
+
+            rank = sorted_bids.index(winning_bid) + 1
+            intra_block_rankings.append(rank)
+
+        return intra_block_rankings
 class Simulation:
     def __init__(self, num_builders, num_blocks):
         self.num_blocks = num_blocks
@@ -137,143 +149,14 @@ class Simulation:
         self.simulate_block()
 
 
-    
-
-    def plot_cumulative_win(self):
-        '''Plot the cumulative win for each strategy'''
-        plt.figure(figsize=(12, 6))
-        strategies_count = {strategy: [0] * self.num_blocks for strategy in STRATEGIES}
-        for i, strategy in enumerate(self.winning_strategy):
-            strategies_count[strategy][i] += 1
-        for strategy, counts in strategies_count.items():
-            plt.plot(range(self.num_blocks), np.cumsum(counts), label=strategy)
-        plt.xlabel('Block Number')
-        plt.ylabel('Cumulative Wins')
-        plt.title('Strategy Trends Over Blocks')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-    def plot_strategy_num(self):
-        """Plot the number of builders for each strategy over blocks."""
-        plt.figure(figsize=(12, 6))
-
-        block_numbers = list(range(len(self.strategy_counts_per_block)))
-        for strategy in STRATEGIES:
-            counts = [count[strategy] for count in self.strategy_counts_per_block]
-            # print(simulation.strategy_counts_per_block)
-            plt.plot(block_numbers, counts, label=strategy)
-
-        plt.title('Number of Builders per Strategy Over Blocks')
-        plt.xlabel('Block Number')
-        plt.ylabel('Number of Builders')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-
-    def plot_bids_for_block(self, block_number):
-        plt.figure(figsize=(12, 6))
-        block_bid_his = self.block_bid_his[block_number]
-
-        # Determine the winning bid and the ID of the winning builder for this block
-        winning_bid = max(bid for counter_bids in block_bid_his for bid in counter_bids.values())
-        winning_builder_id = [builder_id for counter_bids in block_bid_his for builder_id, bid in counter_bids.items() if bid == winning_bid][0]
-
-        # Retrieve the strategies used in this specific block
-        strategies_this_block = self.builder_strategies_per_block[block_number]
-
-        # Generate unique colors for each builder
-        colors = cm.rainbow(np.linspace(0, 1, len(self.builders)))
-
-        for builder, color in zip(self.builders, colors):
-            builder_bids = [block_bid_his[counter].get(builder.id, None) if counter < len(block_bid_his) else None for counter in range(24)]
-            builder_strategy = strategies_this_block[builder.id]
-
-            # Make the winning builder's line bolder
-            linewidth = 2.5 if builder.id == winning_builder_id else 1.0
-            linestyle = '-' if builder.id == winning_builder_id else '--'
-            label = f"Builder {builder.id} ({builder_strategy})"
-            if builder.id == winning_builder_id:
-                label += " - Winner"
-
-            plt.plot(range(24), builder_bids, label=label, color=color, alpha=0.7, linewidth=linewidth, linestyle=linestyle)
-
-        plt.xlabel('Counter')
-        plt.ylabel('Bid Value')
-        plt.title(f'Bids for Block {block_number}')
-        plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
-        plt.grid(True)
-        plt.tight_layout()
-        plt.show()
-
-    def plot_bid_value(self):
-        plt.figure(figsize=(12, 6))
-
-        block_numbers = list(range(len(self.winning_bid)))
-
-        plt.plot(block_numbers, self.winning_bid, label='Winning Bid', color='green')
-        plt.plot(block_numbers, self.winning_block_values, label='Block Value', color='blue')
-
-        plt.xlabel('Block Number')
-        plt.ylabel('Value')
-        plt.title('Winning Bids and Block Values Over Blocks')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-    def intra_rankings(self):
-        intra_block_rankings = []
-
-        for block_bid_his in self.block_bid_his:
-            all_bids = [bid for counter_bids in block_bid_his for bid in counter_bids.values()]
-
-            sorted_bids = sorted(all_bids, reverse=True)
-            winning_bid = max(all_bids)
-
-            rank = sorted_bids.index(winning_bid) + 1
-            intra_block_rankings.append(rank)
-
-        return intra_block_rankings
-
-    def plot_intra_rankings(self):
-        plt.figure(figsize=(12, 6))
-
-        # Calculate intra-block rankings
-        rankings = self.intra_rankings()
-
-        # Plot the rankings
-        plt.plot(range(len(rankings)), rankings, marker='o', linestyle='-')
-
-        plt.xlabel('Block Number')
-        plt.ylabel('Rank of Winning Bid Within Block')
-        plt.title('Intra-Block Ranking of Winning Bids')
-        plt.gca().invert_yaxis()  # Invert y-axis so that rank 1 appears at the top
-        plt.grid(True)
-        plt.tight_layout()
-        plt.show()
-
-    def plot_cumulate_reward_strategy(self):
-        plt.figure(figsize=(12, 6))
-
-        #plot the cumulative reward for each strategy over blocks
-        for strategy in STRATEGIES:
-            cumulate_reward = np.cumsum([self.winning_block_values[i] - self.winning_bid[i] for i in range(len(self.winning_bid)) if self.winning_strategy[i] == strategy])
-            plt.plot(range(len(cumulate_reward)), cumulate_reward, label=strategy)
-
-        plt.xlabel('Block Number')
-        plt.ylabel('Cumulative Reward')
-        plt.title('Cumulative Reward Over Blocks')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
 
 # Run the simulation
 simulation = Simulation(NUM_BUILDERS, NUM_BLOCKS)
 simulation.run()
-# simulation.plot_cumulative_win()
-simulation.plot_strategy_num()
+plotting = Plotting(simulation)
+
+# plotting.plot_cumulative_win(STRATEGIES)
+plotting.plot_strategy_num(STRATEGIES)
 # simulation.plot_bids_for_block(10)
 # simulation.plot_bid_value()
 # simulation.plot_intra_rankings()
