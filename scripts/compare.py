6e5bd0ae1c33806400a68a645841f4b97f08ab70
TammyQAQ <tammywang20020305@gmail.com>
2024-03-20 17:34:02 +0000

    changes

--- scripts/compare.py
+++ scripts/compare.py
@@ -8,7 +8,7 @@ random.seed(42)
 NUM_USERS = 100
 NUM_BUILDERS = 20
 NUM_VALIDATORS = 20
-NUM_PROPOSERS = 5 
+NUM_PROPOSERS = 5
 BLOCK_CAPACITY = 50
 NUM_TRANSACTIONS = 100
 
@@ -19,9 +19,9 @@ class Transaction:
         self.creator_id = creator_id
 
 class Participant:
-    def __init__(self, id, is_mev_oriented):
+    def __init__(self, id, is_mev):
         self.id = id
-        self.is_mev_oriented = is_mev_oriented
+        self.is_mev = is_mev
 
     
 class Builder(Participant):
