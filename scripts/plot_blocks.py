601395d8bd5256b996ce578299c20c64de343b05
Tammy Wang <88000592+TammyQAQ@users.noreply.github.com>
2024-05-08 00:35:07 +0100

    minor change

--- scripts/plot_blocks.py
+++ scripts/plot_blocks.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from os import path
-# import matplotlib as plt
+import matplotlib as plt
 import seaborn as sns
 
 sns.set_theme(style="ticks", palette="pastel")
 
-df1 = pd.read_csv('/Users/tammy/Downloads/posBlocks.csv')
-df2 = pd.read_csv('/Users/tammy/Downloads/pbsBlocks.csv')
+pos_blocks = pd.read_csv('/Users/tammy/Downloads/posBlocks.csv')
+pbs_blocks = pd.read_csv('/Users/tammy/Downloads/pbsBlocks.csv')
 
-print(df1.head())
-print(df2.head())
+print(pos_blocks.head())
+print(pbs_blocks.head())
 
 
+# mev and non mev builder comparison
+sns.boxplot(x='Builder Type', y='Profit', data=pbs_blocks)
+plt.title('Profit Distribution for MEV vs. Non-MEV Builders')
+plt.show()
 
