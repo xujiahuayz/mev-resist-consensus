b17f222c31fbbcb22271b5baf1895bab87513c07
TammyQAQ <tammywang20020305@gmail.com>
2024-07-23 05:36:49 +0100

    fetch

---
+++ fetch/fetch.py
@@ -0,0 +1,69 @@
+import json
+import time
+from web3 import Web3
+
+# Use your Infura project URL
+infura_url = "https://mainnet.infura.io/v3/dd763ae6e7ca4f059f69f4589ad695f0"
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+# Check connection
+if not web3.is_connected():
+    print("Failed to connect to Ethereum node.")
+    exit()
+
+def fetch_block_data(block_number):
+    block = web3.eth.get_block(block_number, full_transactions=True)
+    return block
+
+def get_transaction_data(txn):
+    gas_fee = txn['gasPrice'] * txn['gas']
+    receipt = web3.eth.get_transaction_receipt(txn['hash'])
+    confirm_time = receipt['blockNumber'] - txn['blockNumber']
+    return {
+        'hash': txn['hash'].hex(),
+        'gas_fee': gas_fee,
+        'confirm_time': confirm_time
+    }
+
+def analyze_block(block_number):
+    block = fetch_block_data(block_number)
+    block_size = block['size']
+    transactions = block['transactions']
+    num_transactions = len(transactions)
+    
+    gas_fees = []
+    confirm_times = []
+    
+    for txn in transactions:
+        txn_data = get_transaction_data(txn)
+        gas_fees.append(txn_data['gas_fee'])
+        confirm_times.append(txn_data['confirm_time'])
+    
+    avg_gas_fee = sum(gas_fees) / num_transactions if num_transactions else 0
+    avg_confirm_time = sum(confirm_times) / num_transactions if num_transactions else 0
+    
+    return {
+        'block_number': block_number,
+        'num_transactions': num_transactions,
+        'avg_gas_fee': avg_gas_fee,
+        'avg_confirm_time': avg_confirm_time,
+        'block_size': block_size
+    }
+
+def main():
+    block_number = web3.eth.block_number
+    blocks_to_analyze = 10
+    results = []
+    
+    for i in range(block_number, block_number - blocks_to_analyze, -1):
+        block_data = analyze_block(i)
+        results.append(block_data)
+    
+    # Save results to a JSON file
+    with open('block_analysis.json', 'w') as f:
+        json.dump(results, f, indent=4)
+    
+    print("Block data analysis saved to 'block_analysis.json'")
+
+if __name__ == "__main__":
+    main()
