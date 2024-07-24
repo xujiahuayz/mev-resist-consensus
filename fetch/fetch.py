8f4404d63d96d2b3a2972f4fcdd19585d9582256
Tammy <tammywang2002@gmail.com>
2024-07-24 16:04:47 +0100

    fetch

--- fetch/fetch.py
+++ fetch/fetch.py
@@ -16,17 +16,21 @@ def fetch_block_data(block_number):
     return block
 
 def get_transaction_data(txn):
-    gas_fee = txn['gasPrice'] * txn['gas']
+    gas_fee = txn['gasPrice'] * txn['gas']  # Gas fee in wei
+    gas_fee_eth = web3.from_wei(gas_fee, 'ether')  # Gas fee in Ether for readability
     receipt = web3.eth.get_transaction_receipt(txn['hash'])
     confirm_time = receipt['blockNumber'] - txn['blockNumber']
     return {
         'hash': txn['hash'].hex(),
         'from': txn['from'],
         'to': txn['to'],
-        'value': txn['value'],
+        'value_eth': web3.from_wei(txn['value'], 'ether'),  # Value in Ether
+        'value_wei': txn['value'],
         'gas': txn['gas'],
-        'gas_price': txn['gasPrice'],
-        'gas_fee': gas_fee,
+        'gas_price_gwei': web3.from_wei(txn['gasPrice'], 'gwei'),  # Gas price in Gwei
+        'gas_price_wei': txn['gasPrice'],
+        'gas_fee_wei': gas_fee,
+        'gas_fee_eth': gas_fee_eth,
         'confirm_time': confirm_time,
         'block_number': txn['blockNumber'],
         'transaction_index': txn['transactionIndex'],
@@ -37,7 +41,8 @@ def get_transaction_data(txn):
 
 def analyze_block(block_number):
     block = fetch_block_data(block_number)
-    block_size = block['size']
+    block_size = block['size']  # Block size in bytes
+    miner = block['miner']
     transactions = block['transactions']
     num_transactions = len(transactions)
     
@@ -47,19 +52,22 @@ def analyze_block(block_number):
     
     for txn in transactions:
         txn_data = get_transaction_data(txn)
-        gas_fees.append(txn_data['gas_fee'])
+        gas_fees.append(txn_data['gas_fee_eth'])  # Sum gas fees in Ether
         confirm_times.append(txn_data['confirm_time'])
         transactions_data.append(txn_data)
     
     avg_gas_fee = sum(gas_fees) / num_transactions if num_transactions else 0
     avg_confirm_time = sum(confirm_times) / num_transactions if num_transactions else 0
+    total_gas_fees = sum(gas_fees)  # Total gas fees in Ether
     
     return {
         'block_number': block_number,
+        'miner': miner,
         'num_transactions': num_transactions,
-        'avg_gas_fee': avg_gas_fee,
-        'avg_confirm_time': avg_confirm_time,
-        'block_size': block_size,
+        'avg_gas_fee_eth': avg_gas_fee,
+        'avg_confirm_time_blocks': avg_confirm_time,
+        'block_size_bytes': block_size,
+        'total_gas_fees_eth': total_gas_fees,
         'transactions': transactions_data
     }
 
@@ -77,7 +85,7 @@ def main():
         # Save each block's data to a JSON file
         output_file = os.path.join(output_dir, f'block_{block_data["block_number"]}.json')
         with open(output_file, 'w') as f:
-            json.dump(block_data, f, indent=4)
+            json.dump(block_data, f, indent=4, default=str)
     
     print(f"Block data analysis saved in '{output_dir}'")
 
