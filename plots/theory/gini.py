b201e041d13e3d72fa0f3a399e20818157629e02
Tammy <tammywang2002@gmail.com>
2024-08-07 15:51:51 +0100

    theoretical gini

--- plots/theory/gini.py
+++ plots/theory/gini.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+import os
+
+# Ensure the output directory exists
+os.makedirs('figure', exist_ok=True)
 
 # Function to calculate Gini coefficient
 def gini_coefficient(profits):
@@ -19,28 +23,29 @@ sigma_mev = 2
 mu_gas = 5
 sigma_gas = 1
 
-# Proportions of MEV transactions
-mev_proportions = np.linspace(0, 1, 21)
+# Number of MEV builders/validators
+mev_builders_range = np.arange(1, num_builders + 1)
 
 # Store Gini coefficients
 gini_values = []
 
 # Simulate and calculate Gini coefficients
-for p_mev in mev_proportions:
-    p_non_mev = 1 - p_mev
+for mev_builders in mev_builders_range:
+    non_mev_builders = num_builders - mev_builders
     gini_coeffs = []
     for _ in range(num_simulations):
-        mev_profits = norm.rvs(loc=mu_mev, scale=sigma_mev, size=num_builders)
-        gas_profits = norm.rvs(loc=mu_gas, scale=sigma_gas, size=num_builders)
-        total_profits = p_mev * mev_profits + p_non_mev * gas_profits
+        mev_profits = norm.rvs(loc=mu_mev, scale=sigma_mev, size=mev_builders)
+        gas_profits = norm.rvs(loc=mu_gas, scale=sigma_gas, size=non_mev_builders)
+        total_profits = np.concatenate((mev_profits, gas_profits))
         gini_coeffs.append(gini_coefficient(total_profits))
     gini_values.append(np.mean(gini_coeffs))
 
-# Plot Gini coefficient vs. proportion of MEV transactions
+# Plot Gini coefficient vs. number of MEV builders/validators
 plt.figure(figsize=(10, 6))
-plt.plot(mev_proportions, gini_values, marker='o')
-plt.xlabel('Proportion of MEV Transactions')
+plt.plot(mev_builders_range, gini_values)
+plt.xlabel('Number of MEV Builders/Validators')
 plt.ylabel('Expected Gini Coefficient')
-plt.title('Expected Gini Coefficient vs. Proportion of MEV Transactions')
+plt.title('Expected Gini Coefficient vs. Number of MEV Builders/Validators')
 plt.grid(True)
+plt.savefig('figures/theory/gini.png')
 plt.show()
