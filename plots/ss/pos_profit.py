9518b25c11149f0b246f407590133b2c9c13cc4b
TammyQAQ <tammywang2002@gmail.com>
2025-03-12 11:15:34 +0000

    2*2 pos update

--- plots/ss/pos_profit.py
+++ plots/ss/pos_profit.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 import matplotlib.pyplot as plt
@@ -9,10 +8,11 @@ def load_data(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
-def plot_mev_distribution(aggregated_data, user_attack_count, save_path):
+def plot_mev_distribution(aggregated_data, user_attack_count, save_path, total_users=50, total_validators=20):
     """Generate and save a plot of MEV distribution."""
     # Extracting data
     validator_counts = sorted(int(key) for key in aggregated_data.keys())
+    validator_percentages = [100 * count / total_validators for count in validator_counts]
     validator_mev = [aggregated_data[str(count)]["validators_mev"] for count in validator_counts]
     user_mev = [aggregated_data[str(count)]["users_mev"] for count in validator_counts]
     total_mev = [aggregated_data[str(count)]["total_mev"] for count in validator_counts]
@@ -23,23 +23,33 @@ def plot_mev_distribution(aggregated_data, user_attack_count, save_path):
     uncaptured_mev_percent = [100 - v - u for v, u in zip(validator_mev_percent, user_mev_percent)]
 
     # Set large font sizes
-    axis_font_size = 28
-    title_font_size = 32
-    tick_font_size = 22 
+    axis_font_size = 32
+    title_font_size = 36
+    tick_font_size = 28
 
     plt.figure(figsize=(10, 9))
     palette = sns.color_palette("Blues", 3)
     colors = [palette[2], palette[1], palette[0]]
 
     # Stackplot to visualize MEV distribution with adjusted data
-    plt.stackplot(validator_counts, user_mev_percent, validator_mev_percent, uncaptured_mev_percent,
-                  labels=["Users MEV", "Validators MEV", "Uncaptured MEV"], colors=colors, alpha=0.9)
+    plt.stackplot(validator_percentages, user_mev_percent, validator_mev_percent, uncaptured_mev_percent,
+                  labels=["Users' MEV", "Validators' MEV", "Uncaptured MEV"], colors=colors, alpha=0.9)
     
-    plt.xlabel("Number of Attacking Validators", fontsize=axis_font_size)
+    plt.xlabel("Percentage of Attacking Validators", fontsize=axis_font_size)
     plt.ylabel("MEV Profit Distribution (%)", fontsize=axis_font_size)
-    plt.title(f"User Attacker Number: {user_attack_count}", fontsize=title_font_size)
+    # user_attack_percentage = round(100 * int(user_attack_count) / total_users)
+    user_attack_percentage_map = {
+        '0': '0',
+        '12': '33',
+        '24': '67',
+        '50': '100'
+    }
+    user_attack_percentage = user_attack_percentage_map.get(str(user_attack_count), 'Check Data')
+
+
+    plt.title(f"User Attacker Percentage: {user_attack_percentage}%", fontsize=title_font_size)
     plt.legend(loc="upper right", fontsize=tick_font_size)
-    plt.xticks(ticks=[0, 5, 10, 15, 20], labels=[0, 5, 10, 15, 20], fontsize=tick_font_size)
+    plt.xticks(ticks=[0, 25, 50, 75, 100], labels=['0', '25', '50', '75', '100'], fontsize=tick_font_size)
     plt.yticks(fontsize=tick_font_size)
     plt.margins(0)
     plt.tight_layout(pad=0)
@@ -58,7 +68,7 @@ def main():
             data = load_data(file_path)
             user_attack_count = file_name.split('_')[-1].split('.')[0]  # Extract user attack count from filename
             save_path = os.path.join(output_folder, f"pos_mev_distribution_user_attack_{user_attack_count}.png")
-            plot_mev_distribution(data, user_attack_count, save_path)
+            plot_mev_distribution(data, user_attack_count, save_path, total_users=50)
 
 if __name__ == "__main__":
     main()
